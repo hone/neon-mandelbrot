afba39169b7de89071be101a93cb04105d9b54d2
Terence Lee <hone02@gmail.com>
2017-11-04 18:40:39 -0500

    use more iterators and use vec over ndarray

--- native/src/mandelbrot.rs
+++ native/src/mandelbrot.rs
@@ -1,5 +1,5 @@
-extern crate ndarray;
-use self::ndarray::Array1;
+extern crate time;
+use self::time::PreciseTime;
 
 pub struct Mandelbrot {
     width: usize,
@@ -7,7 +7,7 @@ pub struct Mandelbrot {
 }
 
 impl Mandelbrot {
-    const RADIUS: usize = 2;
+    const RADIUS: f32 = (2 * 2) as f32;
 
     pub fn new(width: usize, height: usize) -> Self {
         Mandelbrot {
@@ -16,37 +16,34 @@ impl Mandelbrot {
         }
     }
 
-    // neon JsArrayType only grabs in u8 views, so need to use u8
-    pub fn generate(&self, max_iterations: usize, scaling_factor: f32, pan_x: f32, pan_y: f32) -> Array1<u8> {
-        let mut result = Array1::zeros([self.width * self.height]);
-        for x in 0..self.width {
-            for y in 0..self.height {
-                let index = x * self.width + y;
-                let c_re = (x as f32 / scaling_factor) - pan_x;
-                let c_im = (y as f32 / scaling_factor) - pan_y;
-                result[index] = self.belongs(c_re, c_im, max_iterations);
-            }
-        }
-
+    pub fn generate(&self, max_iterations: usize, scaling_factor: f32, pan_x: f32, pan_y: f32) -> Vec<u8> {
+        let start_time = PreciseTime::now();
+        let result = (0..self.width * self.height).map(|index| {
+            let x = index / self.width;
+            let y = index - x * self.width;
+            let c_re = (x as f32 / scaling_factor) - pan_x;
+            let c_im = (y as f32 / scaling_factor) - pan_y;
+            self.belongs(c_re, c_im, max_iterations)
+        }).collect();
+        let end_time = PreciseTime::now();
+        eprintln!("Rust timing: {}", start_time.to(end_time));
         result
     }
 
     fn belongs(&self, c_re: f32, c_im: f32, max_iterations: usize) -> u8 {
-        let mut iteration = 0;
         let mut x: f32 = 0.0;
         let mut y: f32 = 0.0;
 
-        while iteration < max_iterations && x * x + y * y <= (Self::RADIUS * Self::RADIUS) as f32 {
+        for iteration in 0..max_iterations {
             let x_new = (x * x - y * y) as f32 + c_re;
             y = (2.0 * x * y) as f32 + c_im;
             x = x_new;
-            iteration += 1;
+
+            if x * x + y * y > Self::RADIUS {
+                return ((iteration as f32 / max_iterations as f32) * 100.0) as u8
+            }
         }
 
-        if iteration < max_iterations {
-            ((iteration as f32 / max_iterations as f32) * 100.0) as u8
-        } else {
-            0 as u8
-        }
+        0 as u8
     }
 }
